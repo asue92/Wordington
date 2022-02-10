0ca78020b1290aa77853538a7a3e3d544d3c59b9
Adam Sue <adam.p.sue@gmail.com>
2022-02-10 12:33:44 -0500

    added check row functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
+const messageDisplay = document.querySelector(".message-container");
 
 const wordington = "SUPER";
 
@@ -51,10 +52,7 @@ guessRows.forEach((guessRow, guessRowIndex) => {
   rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
   guessRow.forEach((guess, guessIndex) => {
     const tileElement = document.createElement("div");
-    tileElement.setAttribute(
-      "id",
-      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
-    );
+    tileElement.setAttribute("id", "guessRow-" + guessRowIndex + "-tile-" + guessIndex);
     tileElement.classList.add("tile");
     rowElement.append(tileElement);
   });
@@ -69,14 +67,52 @@ keys.forEach((key) => {
   keyboard.append(buttonElement);
 });
 
-const handleClick = (key) => {
-  console.log("clicked!", key);
+const handleClick = (letter) => {
+  console.log("clicked!", letter);
+  if (letter === "«") {
+    deleteLetter();
+    return;
+  }
+  if (letter === "ENTER") {
+    checkRow();
+    return;
+  }
   addLetter(letter);
 };
 
 const addLetter = (letter) => {
-  const tile = document.getElementById(
-    "guessRow-" + currentRow + "-tile-" + currentTile
-  );
-  tile.textContent = letter;
+  if (currentTile < 5 && currentRow < 6) {
+    const tile = document.getElementById("guessRow-" + currentRow + "-tile-" + currentTile);
+    tile.textContent = letter;
+    guessRows[currentRow][currentTile] = letter;
+    tile.setAttribute("data", letter);
+    currentTile++;
+  }
+};
+
+const deleteLetter = () => {
+  if (currentTile > 0) {
+    currentTile--;
+    const tile = document.getElementById("guessRow-" + currentRow + "-tile-" + currentTile);
+    tile.textContent = "";
+    guessRows[currentRow][currentTile] = "";
+    tile.setAttribute("data", "");
+  }
+};
+
+const checkRow = () => {
+  const guess = guessRows[currentRow].join("");
+  if (currentTile === 5) {
+    console.log("guess is " + guess, "wordington is " + wordington);
+    if (wordington === guess) {
+      showMessage("Magnificent!");
+    }
+  }
+};
+
+const showMessage = (message) => {
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
 };
